5f5b4945b62c5368e7a9725c6d7165f44ec25fd0
i9-9 <yvan.vrs@gmail.com>
2024-08-23 10:17:37 -0300

    Añadir archivos iniciales al repositorio

---
+++ .next/server/app/favicon.ico/route.js
@@ -0,0 +1,141 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 155;
+exports.ids = [155];
+exports.modules = {
+
+/***/ 7783:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/@edge-runtime/cookies");
+
+/***/ }),
+
+/***/ 8530:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/@opentelemetry/api");
+
+/***/ }),
+
+/***/ 4426:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/chalk");
+
+/***/ }),
+
+/***/ 252:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/cookie");
+
+/***/ }),
+
+/***/ 2196:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/ua-parser-js");
+
+/***/ }),
+
+/***/ 4021:
+/***/ ((module) => {
+
+module.exports = import("next/dist/compiled/@vercel/og/index.node.js");;
+
+/***/ }),
+
+/***/ 8255:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  headerHooks: () => (/* binding */ headerHooks),
+  originalPathname: () => (/* binding */ originalPathname),
+  requestAsyncStorage: () => (/* binding */ requestAsyncStorage),
+  routeModule: () => (/* binding */ routeModule),
+  serverHooks: () => (/* binding */ serverHooks),
+  staticGenerationAsyncStorage: () => (/* binding */ staticGenerationAsyncStorage),
+  staticGenerationBailout: () => (/* binding */ staticGenerationBailout)
+});
+
+// NAMESPACE OBJECT: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Ffavicon.ico%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!./app/favicon.ico?__next_metadata__
+var favicon_next_metadata_namespaceObject = {};
+__webpack_require__.r(favicon_next_metadata_namespaceObject);
+__webpack_require__.d(favicon_next_metadata_namespaceObject, {
+  GET: () => (GET),
+  dynamic: () => (dynamic)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/node-polyfill-headers.js
+var node_polyfill_headers = __webpack_require__(5387);
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-modules/app-route/module.js
+var app_route_module = __webpack_require__(9267);
+var module_default = /*#__PURE__*/__webpack_require__.n(app_route_module);
+// EXTERNAL MODULE: ./node_modules/next/server.js
+var server = __webpack_require__(4664);
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Ffavicon.ico%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!./app/favicon.ico?__next_metadata__
+
+
+const contentType = "image/x-icon"
+const buffer = Buffer.from("AAABAAEAICAAAAEAIACoEAAAFgAAACgAAAAgAAAAQAAAAAEAIAAAAAAAABAAACMuAAAjLgAAAAAAAAAAAAAFBQX/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BQUF/wYGBv8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BgYG/wYGBv8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BgYG/wYGBv8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8GBgb/BwcH/wYGBv8HBwf/BgYG/wUFBf8GBgb/BwcH/wYGBv8FBQX/BwcH/wcHB/8GBgb/BwcH/wcHB/8GBgb/BQUF/wYGBv8HBwf/BgYG/wYGBv8HBwf/BwcH/wYGBv8GBgb/BgYG/wcHB/8HBwf/BgYG/wYGBv8HBwf/DQ0N/xMTE/8ICAj/GRkZ/wsLC/8XFxf/Kysr/xAQEP8FBQX/Ghoa/ykpKf8PDw//BgYG/xQUFP8MDAz/BgYG/xcXF/8pKSn/Dw8P/w4ODv8SEhL/FBQU/xEREf8SEhL/FxcX/yEhIf8gICD/Dg4O/wcHB/8GBgb/BgYG/wQEBP9FRUX/cnJy/0VFRf+Tk5P/ODg4/56env+ampr/jY2N/zo6Ov+YmJj/kpKS/4CAgP8NDQ3/goKC/zc3N/8cHBz/np6e/6CgoP+Dg4P/UVFR/3l5ef+dnZ3/c3Nz/2tra/+RkZH/np6e/4qKiv8rKyv/BQUF/wYGBv8GBgb/AwMD/09PT/+/v7//sLCw/0RERP9ubm7/eHh4/wwMDP+bm5v/YmJi/2BgYP9/f3//paWl/xMTE/+ZmZn/Pz8//0FBQf+cnJz/JCQk/6ioqP9ubm7/p6en/6qqqv+ioqL/fX19/6Ojo/91dXX/SkpK/xAQEP8GBgb/BgYG/wYGBv8DAwP/TU1N/9XV1f+xsbH/GBgY/3Fxcf92dnb/CQkJ/5iYmP96enr/ra2t/3d3d/9RUVH/Dg4O/5ycnP9DQ0P/R0dH/5WVlf8aGhr/pqam/3BwcP/Kysr/aWlp/7y8vP+AgID/pqam/5GRkf9mZmb/EhIS/wYGBv8GBgb/BgYG/wQEBP9GRkb/enp6/2dnZ/92dnb/MjIy/6Ghof+YmJj/kpKS/z09Pf+enp7/lpaW/3Fxcf9ycnL/v7+//5+fn/9ubm7/gYGB/xsbG/+QkJD/YmJi/7y8vP8uLi7/pqam/3V1df+Tk5P/nZ2d/4SEhP8hISH/BQUF/wYGBv8GBgb/BwcH/w4ODv8UFBT/CgoK/x0dHf8JCQn/Ghoa/zAwMP8TExP/BQUF/x4eHv8sLCz/Dg4O/x0dHf8iIiL/IyMj/xYWFv9MTEz/Xl5e/zo6Ov8QEBD/Gxsb/wgICP8YGBj/FBQU/xkZGf8lJSX/IyMj/w0NDf8HBwf/BgYG/wYGBv8HBwf/BgYG/wYGBv8HBwf/BgYG/wcHB/8JCQn/CAgI/wcHB/8MDAz/BgYG/wkJCf8KCgr/CgoK/wUFBf8SEhL/ERER/xISEv8aGhr/ERER/wkJCf8ICAj/FxcX/wsLC/8FBQX/BgYG/wUFBf8FBQX/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8HBwf/BAQE/0xMTP9dXV3/ExMT/3x8fP8VFRX/ZGRk/1tbW/91dXX/UlJS/5eXl/+VlZX/QUFB/wAAAP9SUlL/XV1d/3V1df+VlZX/iIiI/xoaGv8GBgb/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8HBwf/BwcH/wcHB/8DAwP/ZWVl/3x8fP8WFhb/ra2t/0FBQf+YmJj/eHh4/5+fn/9TU1P/XFxc/5mZmf98fHz/AAAA/21tbf99fX3/Xl5e/1xcXP+4uLj/ODg4/wQEBP8HBwf/BwcH/wcHB/8HBwf/BgYG/wYGBv8HBwf/BwcH/wcHB/8HBwf/BwcH/wICAv9kZGT/e3t7/xUVFf+8vLz/k5OT/8HBwf93d3f/nZ2d/25ubv+kpKT/gICA/zIyMv8BAQH/bW1t/319ff+Tk5P/k5OT/25ubv8UFBT/BgYG/wcHB/8HBwf/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8FBQX/RUVF/62trf+4uLj/ZWVl/52dnf8dHR3/f39//3Jycv+Tk5P/dXV1/5mZmf+VlZX/QkJC/wAAAP9mZmb/eHh4/5mZmf+IiIj/jo6O/xcXF/8GBgb/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8HBwf/BwcH/wYGBv8iIiL/Ojo6/zo6Ov8rKyv/JiYm/wsLC/8gICD/HR0d/yQkJP8SEhL/QUFB/zg4OP8LCwv/BgYG/xsbG/8bGxv/ICAg/0hISP8lJSX/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BgYG/wYGBv8HBwf/BwcH/wcHB/8HBwf/BwcH/wUFBf8EBAT/BAQE/wUFBf8FBQX/BwcH/wUFBf8GBgb/BQUF/wYGBv8EBAT/BAQE/wcHB/8HBwf/BgYG/wYGBv8FBQX/AwMD/wUFBf8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BgYG/wYGBv8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BgYG/wYGBv8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8GBgb/BgYG/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wYGBv8GBgb/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BwcH/wcHB/8HBwf/BgYG/wUFBf8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8GBgb/BgYG/wYGBv8FBQX/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=", 'base64'
+  )
+
+function GET() {
+  return new server.NextResponse(buffer, {
+    headers: {
+      'Content-Type': contentType,
+      'Cache-Control': "public, max-age=0, must-revalidate",
+    },
+  })
+}
+
+const dynamic = 'force-static'
+
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-app-loader.js?page=%2Ffavicon.ico%2Froute&name=app%2Ffavicon.ico%2Froute&pagePath=private-next-app-dir%2Ffavicon.ico&appDir=%2FUsers%2Fivan%2FDocuments%2FCodigo%2Fkostume-location%2Fapp&appPaths=%2Ffavicon.ico&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&basePath=&assetPrefix=&nextConfigOutput=export&preferredRegion=&middlewareConfig=e30%3D!
+
+    
+
+    
+
+    
+
+    const options = {"definition":{"kind":"APP_ROUTE","page":"/favicon.ico/route","pathname":"/favicon.ico","filename":"favicon","bundlePath":"app/favicon.ico/route"},"resolvedPagePath":"next-metadata-route-loader?page=%2Ffavicon.ico%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!/Users/ivan/Documents/Codigo/kostume-location/app/favicon.ico?__next_metadata__","nextConfigOutput":"export"}
+    const routeModule = new (module_default())({
+      ...options,
+      userland: favicon_next_metadata_namespaceObject,
+    })
+
+    // Pull out the exports that we need to expose from the module. This should
+    // be eliminated when we've moved the other routes to the new format. These
+    // are used to hook into the route.
+    const {
+      requestAsyncStorage,
+      staticGenerationAsyncStorage,
+      serverHooks,
+      headerHooks,
+      staticGenerationBailout
+    } = routeModule
+
+    const originalPathname = "/favicon.ico/route"
+
+    
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [381,572], () => (__webpack_exec__(8255)));
+module.exports = __webpack_exports__;
+
+})();
